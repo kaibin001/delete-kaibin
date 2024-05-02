2e74fb87c5661deb6c874f619978ef517e2280d4
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-05-03 00:24:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = load_model('model')
 fight_events_path = "data-v2.csv"
 fight_events = pd.read_csv(fight_events_path)
 
-upcoming_events_path = "test-predictions.csv"
+upcoming_events_path = "upcoming_events.csv"
 upcoming_events = pd.read_csv(upcoming_events_path)
 
 st.title("UFC Fight Predictor")
@@ -20,14 +20,15 @@ st.sidebar.write("Upcoming Events", upcoming_events[['Matchup']])
 # Fighter Selection
 fighter_names = sorted(fight_events['Fighter1'].dropna().unique())
 fighter1 = st.selectbox("Select Fighter 1", [''] + fighter_names)
-fighter1last5 = fight_events[(fight_events['Fighter1'] == fighter1)].rename(columns={'Fighter2': 'Fighter','Win/Loss (Fighter1)': 'Result'}).drop(columns=['Fighter1', 'Weight Class']) 
+fighter1last5 = fight_events[(fight_events['Fighter1'] == fighter1)].rename(columns={'Fighter2': 'Fighter','Win/Loss (Fighter1)': 'Result'}).drop(columns=['Fighter1', 'Weight Class']).head(5)
 if fighter1:
     st.write(f"{fighter1} Last 5 Fights", fighter1last5, index=False)
 
 fighter2 = st.selectbox("Select Fighter 2", [''] + [f for f in fighter_names if f != fighter1])
-fighter2last5 = fight_events[(fight_events['Fighter1'] == fighter2)].rename(columns={'Fighter2': 'Fighter','Win/Loss (Fighter1)': 'Result'}).drop(columns=['Fighter1', 'Weight Class'])
+fighter2last5 = fight_events[(fight_events['Fighter1'] == fighter2)].rename(columns={'Fighter2': 'Fighter','Win/Loss (Fighter1)': 'Result'}).drop(columns=['Fighter1', 'Weight Class']).head(5)
 if fighter2:
     st.write(f"{fighter2} Last 5 Fights", fighter2last5, index=False)
+    
     
 # Prediction Button
 if st.button("Predict Winner"):
